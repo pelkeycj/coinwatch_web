8f45cea2a33b9343833d582853941a9f3ad695b0
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-11-08 04:28:16 -0500

    adds wrapper for api requests

--- src/api.js
+++ src/api.js
@@ -1,6 +1,59 @@
+// wrappers for fetch methods
+
+const API = process.env.REACT_APP_API_URL;
+
+function headers() {
+  const token = JSON.parse(localStorage.getItem('token'));
+  const auth = 'Bearer: '.concat(token);
+  return {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: auth,
+  };
+}
+
+function parseResponse(resp) {
+  return resp.json().then((json) => {
+    if (!resp.ok) {
+      return Promise.reject(json);
+    }
+    return json;
+  });
+}
+
+
+function queryString(params) {
+  const query = Object.keys(params)
+    .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
+    .join('&');
+  return `${query.length ? '?' : ''}${query}`;
+}
 
 export default {
-  // TODO get
-  //TODO post
-  //TODO delete?
+  get(url, params = {}) {
+    return fetch(`${API}${url}${queryString(params)}`, {
+      method: 'GET',
+      headers: headers(),
+    })
+      .then(parseResponse);
+  },
+
+  post(url, data) {
+    const body = JSON.stringify(data);
+
+    return fetch(`${API}${url}`, {
+      method: 'POST',
+      headers: headers(),
+      body,
+    })
+      .then(parseResponse);
+  },
+
+  delete(url) {
+    return fetch(`${API}${url}`, {
+      method: 'DELETE',
+      headers: headers(),
+    })
+      .then(parseResponse);
+  },
 };
