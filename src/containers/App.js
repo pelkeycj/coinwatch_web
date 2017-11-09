208fb4d68e5d98e65c28b5854235388d9ad4c281
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-11-09 02:22:39 -0500

    add signin via api session

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
+import Signin from './Signin';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/signin" component={Signin} />
         </Switch>
       </BrowserRouter>
     );
