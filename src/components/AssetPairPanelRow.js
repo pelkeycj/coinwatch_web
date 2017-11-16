6fcb6467913d29d1dde2c45b1e00f395c9bb127b
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-11-15 19:51:14 -0500

    align rates- causes issues when viewed half screen

--- src/components/AssetPairPanelRow.js
+++ src/components/AssetPairPanelRow.js
@@ -40,7 +40,6 @@ class AssetPairPanelRow extends React.Component {
   }
 
   handleClick() {
-    console.log('clicked');
     this.props.handleSubmit(this.props.market);
   }
 
@@ -55,7 +54,7 @@ class AssetPairPanelRow extends React.Component {
 
     return (
       <Row align="center">
-        <Col md={12}>
+        <Col md={1}>
           <Button
             type="button"
             style={{ display: 'inline' }}
@@ -64,10 +63,29 @@ class AssetPairPanelRow extends React.Component {
           >
             <span className={glyph} />
           </Button>
+        </Col>
+        <Col md={5}>
+
+        <Link className={css(styles.link)} to={{ pathname:"/chart/markets", state: { markets: [market] }}}>
+          <p className={css(styles.market)}>{market.exchange.toUpperCase() + ': '}</p>
+        </Link>
+        </Col>
+
+        <Col md={6} style={{ textAlign: 'left'}}>
+        <Link className={css(styles.link)} to={{ pathname:"/chart/markets", state: { markets: [market] }}}>
+          <p className={css(styles.market)}>{ market.rate}</p>
+
+        </Link>
+        </Col>
+          {/*
           <Link className={css(styles.link)} to={{ pathname:"/chart/markets", state: { markets: [market] }}}>
             <p className={css(styles.market)}>{market.exchange.toUpperCase() + ': ' + market.rate}</p>
+
           </Link>
         </Col>
+        */}
+
+
       </Row>
     );
   }
